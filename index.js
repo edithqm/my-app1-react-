7d3670ff363a73fe4a6046f26db81cbc889c0f51
edith stefany <edistefan416@gmail.com>
2020-08-21 09:32:20 -0500

    mi publicacion

---
+++ index.js
@@ -0,0 +1,62 @@
+var edad= 10
+
+if (true){
+
+    let edad=20
+     const edad 
+    console.log(edad)
+
+}
+const persona={
+    nombre:'juanito',
+    edad:20
+}
+persona.edad =21
+persona.pais='peru'
+console.log(persona)
+
+function sumar (num){
+    console.log(10)
+}
+sumar(10)
+const sumaDos=(num1 num2 ) =>{
+    return (num1. num2)   
+}                                                                                                             
+const resultado= sumarDos(20,30)
+  console.log(resultado) 
+function sumar (num)
+
+    const mensaje=()=> {   
+        return 'hola soy'- nombre
+
+}
+const resultadodos = mensaje ('juanito') 
+console.log (resultadodos)
+
+const sumatres=  (num =1) =>{
+console.log (mun=3)
+}
+sumatres(10)
+
+const numero =(num)=>(
+    return`el numero es:$[num1+mun2 ]`
+    )
+const resultados =numero(10.20)
+console.log(resultado)
+
+
+const mascota={
+    nombre:'tom',
+    edad: 10,
+    vivo:true,
+    razas [peluche,negro]
+
+}
+
+console.log(mascota)
+console.log(mascota.nombre)
+console.log(mascota.edad)
+console.log(mascota.vivo)
+
+mascota.id=1
+console.log(mascota.id)
